75786fed49c9369e4e957bc3264596305ad0f3f5
georgy-kirilov <georgynk17@gmail.com>
2021-10-15 22:07:31 +0300

    Add Validation Attributes inside RegisterInputModel class

--- src/WebApi/SuperScrabble.InputModels/RegisterInputModel.cs
+++ src/WebApi/SuperScrabble.InputModels/RegisterInputModel.cs
@@ -1,27 +1,82 @@
 ﻿namespace SuperScrabble.InputModels
 {
+    using SuperScrabble.CustomValidationAttributes;
     using SuperScrabble.LanguageResources;
+
     using System.ComponentModel.DataAnnotations;
 
     using static SuperScrabble.LanguageResources.ResourceNames;
+    using static SuperScrabble.Common.ModelValidationConstraints.Password;
 
     public class RegisterInputModel
     {
+        // UserName
+
         [Display(Name = UserNameDisplayName, ResourceType = typeof(Resource))]
-        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = UserNameIsRequired)]
+
+        [Required(
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = UserNameIsRequired)]
+
         public string UserName { get; init; }
 
+        // Email Address
+
         [Display(Name = "EmailAddressDisplayName", ResourceType = typeof(Resource))]
-        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "EmailAddressIsRequired")]
-        [EmailAddress(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "EmailAddressIsInvalid")]
+
+        [Required(
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "EmailAddressIsRequired")]
+
+        [EmailAddress(
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "EmailAddressIsInvalid")]
+
         public string Email { get; init; }
 
+        // Password
+
         [Display(Name = PasswordDisplayName, ResourceType = typeof(Resource))]
-        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = PasswordIsRequired)]
+
+        [Required(
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = PasswordIsRequired)]
+
+        [PasswordMinLength(
+            MinLength,
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "PasswordIsTooShort")]
+
+        [RequireDigit(
+            RequireDigit,
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "PasswordRequiresDigit")]
+
+        [RequireLowercase(
+            RequireLowercase,
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "PasswordRequiresLowercase")]
+
+        [RequireUppercase(
+            RequireUppercase,
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "PasswordRequiresUppercase")]
+
+        [RequireNonAlphanumeric(
+            RequireNonAlphanumeric,
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "PasswordRequiresNonAlphanumeric")]
+
         public string Password { get; init; }
 
+        // Repeated Password
+
         [Display(Name = "RepeatedPasswordDisplayName", ResourceType = typeof(Resource))]
-        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "RepeatedPasswordIsRequired")]
+
+        [Required(
+            ErrorMessageResourceType = typeof(Resource),
+            ErrorMessageResourceName = "RepeatedPasswordIsRequired")]
+
         public string RepeatedPassword { get; init; }
     }
 }
