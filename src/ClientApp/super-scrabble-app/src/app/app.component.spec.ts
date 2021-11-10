0ef2caa9b39ca1cfa7f7a943d5fa3d0135252caf
georgy-kirilov <georgynk17@gmail.com>
2021-11-10 16:01:09 +0200

    Create up-to-date Angular super-scrabble-app

--- src/ClientApp/super-scrabble-app/src/app/app.component.spec.ts
+++ src/ClientApp/super-scrabble-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client-app'`, () => {
+  it(`should have as title 'super-scrabble-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client-app');
+    expect(app.title).toEqual('super-scrabble-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('client-app app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('super-scrabble-app app is running!');
   });
 });
