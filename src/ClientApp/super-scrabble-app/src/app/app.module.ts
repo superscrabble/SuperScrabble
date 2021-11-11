0c94d710ca61ba4fe4c99b1375bcf1cbbd0456e0
georgy-kirilov <georgynk17@gmail.com>
2021-11-11 11:12:31 +0200

    Fix angular boostrap set-up

--- src/ClientApp/super-scrabble-app/src/app/app.module.ts
+++ src/ClientApp/super-scrabble-app/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './pages/register-form/register-form.component';
@@ -12,7 +17,10 @@ import { RegisterFormComponent } from './pages/register-form/register-form.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
