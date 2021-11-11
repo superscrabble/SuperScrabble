6054aefc17c8c0df15eedd1725789f6b35413966
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-11-11 13:33:40 +0200

    Refactor LoginForm and RegisterForm

--- src/ClientApp/super-scrabble-app/src/app/pages/register-form/register-form.component.ts
+++ src/ClientApp/super-scrabble-app/src/app/pages/register-form/register-form.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ErrorHandler } from 'src/app/services/error-handler';
+import { WebRequestsService } from 'src/app/services/web-requests.service';
 
 @Component({
   selector: 'app-register-form',
@@ -21,7 +22,7 @@ export class RegisterFormComponent implements OnInit {
 
   propertyErrorMessages = new Map();
 
-  constructor(private http: HttpClient, private router: Router, private errorHandler: ErrorHandler) {}
+  constructor(private webRequestsService: WebRequestsService, private router: Router, private errorHandler: ErrorHandler) {}
 
   ngOnInit(): void {}
 
@@ -42,35 +43,39 @@ export class RegisterFormComponent implements OnInit {
   }
 
   onRegister(): void {
-
     this.input.username = this.registerForm.value.username;
     this.input.email = this.registerForm.value.email;
     this.input.password = this.registerForm.value.password;
     this.input.repeatedPassword = this.registerForm.value.repeatedPassword;
 
-    const url = 'https://localhost:5001/api/users/register';
+    const url = 'api/users/register';
 
     this.propertyErrorMessages = new Map();
     
-    this.http.post(url, this.input, 
-                  { headers: {"Content-Type": "application/json"}, observe: 'response', responseType: 'text' })
-                  .subscribe((res: HttpResponse<any>) => {
-      let data = JSON.parse(res.body);
-      localStorage.setItem("access_token", data["token"]);
-      this.router.navigate(['/']);
-    },
-    error => {
-      if (error.status == 400) {
-        const errors = JSON.parse(error.error);
-        for (let i = 0; i < errors.length; i++) {
-          let propertyName = errors[i].propertyName;
-          let errorMessages = errors[i].errorMessages;
-          this.propertyErrorMessages.set(propertyName, errorMessages);
-        }
-      }
-      else {
-        this.errorHandler.handle(error.status);
-      }
+    this.webRequestsService.post(url, this.input)
+    .subscribe({
+      next: this.handleRegisterResponse.bind(this),
+      error: this.handleError.bind(this)
     });
+  }
+
+  handleRegisterResponse(res: HttpResponse<any>): void {
+    let data = JSON.parse(res.body);
+    localStorage.setItem("access_token", data["token"]);
+    this.router.navigate(['/']);
+  }
+
+  handleError(error: any): void {
+    if (error.status == 400) {
+      const errors = JSON.parse(error.error);
+      for (let i = 0; i < errors.length; i++) {
+        let propertyName = errors[i].propertyName;
+        let errorMessages = errors[i].errorMessages;
+        this.propertyErrorMessages.set(propertyName, errorMessages);
+      }
+    }
+    else {
+      this.errorHandler.handle(error.status);
+    }
   }
 }
---
+++ src/ClientApp/super-scrabble-app/src/app/services/web-requests.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebRequestsService {
+  
+  readonly ROOT_URL;
+
+  constructor(private http: HttpClient) {
+    this.ROOT_URL = "https://localhost:5001";
+  }
+
+  get(url: string) {
+    return this.http.get(`${this.ROOT_URL}/${url}`, { observe: 'response', responseType: 'text' });
+  }
+
+  getWithHeaders(url: string, headers: any) {
+    return this.http.get(`${this.ROOT_URL}/${url}`, { headers: {"Content-Type": "application/json"}, observe: 'response', responseType: 'text' });
+  }
+
+  getAuthorized(url: string) {
+    return this.http.get(`${this.ROOT_URL}/${url}`, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }, observe: 'response', responseType: 'text' });
+  }
+  
+  post(url: string, payload: object) {
+    return this.http.post(`${this.ROOT_URL}/${url}`, payload, { headers: {"Content-Type": "application/json"}, observe: 'response', responseType: 'text' });
+  }
+
+  postAuthorized(url: string, payload: object) {
+    return this.http.post(`${this.ROOT_URL}/${url}`, payload, { headers: {"Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('access_token')}, observe: 'response', responseType: 'text' });
+  }
+}
