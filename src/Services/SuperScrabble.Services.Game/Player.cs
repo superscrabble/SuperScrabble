89756036f6a56f4a101440fc51ac89fea5447ed2
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-11-18 17:24:31 +0200

    Add Tile

---
+++ src/Services/SuperScrabble.Services.Game/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperScrabble.Services.Game
+{
+    class Player
+    {
+        public string UserName { get; set; }
+        public int Points { get; set; }
+        public IList<Tile> Tiles { get; set; }
+
+        public void AddTile(Tile tile)
+        {
+            Tiles.Add(tile);
+        }
+
+        public void RemoveTile(Tile tile)
+        {
+            Tiles.Remove(tile);
+        }
+    }
+}
---
+++ src/Services/SuperScrabble.Services.Game/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SuperScrabble.Services.Game
+{
+    public class Tile
+    {
+        public char Letter { get; set; }
+        public int Points { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(obj != null && obj is Tile)
+            {
+                Tile other = obj as Tile;
+                return other.Letter == this.Letter && other.Points == this.Points;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Letter, Points);
+        }
+    }
+}
