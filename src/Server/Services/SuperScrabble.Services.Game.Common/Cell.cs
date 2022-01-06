2e11de4a390a461dc7de5f6e1711f9eca075cf1e
Georgi Kirilov <georgynk17@gmail.com>
2022-01-06 14:42:02 +0200

    Some code changes

---
+++ src/Server/Services/SuperScrabble.Services.Game.Common/Cell.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperScrabble.Services.Game.Common
+{
+    public class Cell
+    {
+        public Cell(CellType type, Tile? tile = null)
+        {
+            this.Type = type;
+            this.Tile = tile;
+        }
+
+        public CellType Type { get; }
+
+        public Tile? Tile { get; set; }
+    }
+}
---
+++ src/Server/Services/SuperScrabble.Services.Game.Models/IBoard.cs
@@ -0,0 +1,26 @@
+﻿namespace SuperScrabble.Services.Game.Models
+{
+    using SuperScrabble.Common;
+    using SuperScrabble.Services.Game.Common;
+
+    public interface IBoard
+    {
+        int Height { get; }
+
+        int Width { get; }
+
+        Cell this[int row, int column] { get; set; }
+
+        Cell this[Position position] { get; set; }
+
+        void FreeCell(Position position);
+
+        bool IsCellFree(Position position);
+
+        bool IsPositionInside(Position position);
+
+        bool IsEmpty();
+
+        bool IsPositionCenter(Position position);
+    }
+}
