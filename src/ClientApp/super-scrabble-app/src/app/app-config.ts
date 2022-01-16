b5276f70c60eaafc8539e08ac2a4c78faf4a1a19
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-01-16 19:31:26 +0200

    Add Config file

--- src/ClientApp/super-scrabble-app/src/app/app-config.ts
+++ src/ClientApp/super-scrabble-app/src/app/app-config.ts
@@ -1,3 +1,5 @@
 export class AppConfig {
     public static WildcardSymbol: string = "\u0000";
+    public static BoardCellMinWidth: number = 35.2; //2.2rem = 35.2px
+    public static BoardCellMinHeight: number = 35.2; //2.2rem = 35.2px
 }
