19adf9f7518a3190f552d64b656838ac6153fd28
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-01-16 19:39:45 +0200

    Add imports so that it matches the current version

--- src/ClientApp/super-scrabble-app/src/app/app.module.ts
+++ src/ClientApp/super-scrabble-app/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { RegisterFormComponent } from './pages/register-form/register-form.compo
 import { LoginFormComponent } from './pages/login-form/login-form.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
-import { ErrorDialog, ChangeWildcardDialog, LeaveGameDialog, FormatTimePipe, SettingsDialog } from './pages/game/game.component';
+import { FormatTimePipe } from './pages/game/game.component';
 import { Utilities } from './common/utilities';
 import { GameComponent } from './pages/game/game.component';
 import { GameSummaryComponent } from './pages/game-summary/game-summary.component';
@@ -23,6 +23,12 @@ import { ScoreboardComponent } from './pages/common/scoreboard/scoreboard.compon
 import { WordInfoComponent } from './pages/common/word-info/word-info.component';
 import { GameLogsComponent } from './pages/common/game-logs/game-logs.component';
 import { GameboardComponent } from './pages/common/gameboard/gameboard.component';
+import { ErrorDialogComponent } from './pages/common/dialogs/error-dialog/error-dialog.component';
+import { SettingsDialogComponent } from './pages/common/dialogs/settings-dialog/settings-dialog.component';
+import { ChangeWildcardDialogComponent } from './pages/common/dialogs/change-wildcard-dialog/change-wildcard-dialog.component';
+import { LeaveGameDialogComponent } from './pages/common/dialogs/leave-game-dialog/leave-game-dialog.component';
+import { ChangeLettersDialogComponent } from './pages/common/dialogs/change-letters-dialog/change-letters-dialog.component';
+import { GameContentDialogComponent } from './pages/common/dialogs/game-content-dialog/game-content-dialog.component';
 
 @NgModule({
   declarations: [
@@ -33,15 +39,17 @@ import { GameboardComponent } from './pages/common/gameboard/gameboard.component
     NavbarComponent,
     GameComponent,
     GameSummaryComponent,
-    ErrorDialog,
-    ChangeWildcardDialog,
-    LeaveGameDialog,
     FormatTimePipe,
-    SettingsDialog,
     ScoreboardComponent,
     WordInfoComponent,
     GameLogsComponent,
-    GameboardComponent
+    GameboardComponent,
+    ErrorDialogComponent,
+    SettingsDialogComponent,
+    ChangeWildcardDialogComponent,
+    LeaveGameDialogComponent,
+    ChangeLettersDialogComponent,
+    GameContentDialogComponent
   ],
   imports: [
     CommonModule,
