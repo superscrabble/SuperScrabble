e950d961b7ffa0ebf4ce93632107fe1f9844cbaf
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-01-16 19:30:21 +0200

    Add game-logs component

--- src/ClientApp/super-scrabble-app/src/app/pages/common/game-logs/game-logs.component.ts
+++ src/ClientApp/super-scrabble-app/src/app/pages/common/game-logs/game-logs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Action } from 'src/app/models/action';
 
 @Component({
   selector: 'app-game-logs',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameLogsComponent implements OnInit {
 
+  @Output('showWordMeaningOf') _showWordMeaningOf: EventEmitter<any> = new EventEmitter();
+  @Input() gameLogs: Action[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showWordMeaningOf(value: string) {
+    this._showWordMeaningOf.emit(value);
   }
 
 }
