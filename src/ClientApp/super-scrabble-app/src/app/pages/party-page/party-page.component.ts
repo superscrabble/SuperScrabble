350c72ebd20febf0a00dfc0dd381865094f2ce72
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-02-08 19:10:57 +0200

    Add matchProps changes

--- src/ClientApp/super-scrabble-app/src/app/pages/party-page/party-page.component.ts
+++ src/ClientApp/super-scrabble-app/src/app/pages/party-page/party-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerType } from 'src/app/common/enums/timer-type';
 import { GameConfig } from 'src/app/models/game-configuaration/game-config';
 import { SignalrService } from 'src/app/services/signalr.service';
 
@@ -10,6 +11,14 @@ class PartyData {
   members: string[] = [];
 }
 
+class MatchProp {
+  name: string = "";
+  values: {
+    key: string;
+    value: number;
+  }[] = []
+}
+
 @Component({
   selector: 'app-party-page',
   templateUrl: './party-page.component.html',
@@ -19,13 +28,48 @@ export class PartyPageComponent implements OnInit {
   
   partyData: PartyData = new PartyData();
   isOwner: boolean = false;
-  
+  matchProps: MatchProp[] = [];
+  selectedMatchProps: Map<string, number> = new Map();
+
   constructor(private signalrService: SignalrService) {
     this.partyData.invitationCode = "DSDS121"
     this.partyData.members = ["Denis", "Gosho", "Misho", "Pesho"]
     //this.partyData.members = ["Denis"]
     this.partyData.owner = "Denis";
     this.isOwner = true;
+
+    this.matchProps = [
+      {
+        name: "TimerType",
+        values: [
+          {
+            key: "Standard",
+            value: 1
+          },
+          {
+            key: "Chess",
+            value: 2
+          }
+        ]
+      },
+      {
+        name: "TimerDifficulty",
+        values: [
+          {
+            key: "Slow",
+            value: 1
+          },
+          {
+            key: "Standard",
+            value: 2
+          },
+          {
+            key: "Fast",
+            value: 3
+          }
+        ]
+      }
+    ]
   }
 
   ngOnInit(): void {
@@ -41,6 +85,10 @@ export class PartyPageComponent implements OnInit {
     })
   }
 
+  selectMatchProp(key: string, value: number) {
+    this.selectedMatchProps.set(key, value);
+  }
+
   isPartyOwner(member: string) {
     return this.partyData.owner == member;
   }
@@ -48,6 +96,17 @@ export class PartyPageComponent implements OnInit {
   isPartyReady() : boolean {
     if(this.partyData.members.length >= 2) {
       return true;
+    }
+    return false;
+  }
+
+  startGame() {
+    console.log(this.selectedMatchProps);
+  }
+
+  isMatchPropOptionSelected(name: string, value: number) : boolean {
+    if(this.selectedMatchProps.has(name)) {
+      return this.selectedMatchProps.get(name) == value;
     }
     return false;
   }
