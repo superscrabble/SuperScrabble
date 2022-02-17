5d0df6c6732ab09892562df5156b01d9e64451d7
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-02-17 13:47:56 +0200

    Install angular/fire

--- src/ClientApp/super-scrabble-app/src/environments/environment.ts
+++ src/ClientApp/super-scrabble-app/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'superscrabble-ef26c',
+    appId: '1:735949795375:web:3391aabae366f3002a96da',
+    storageBucket: 'superscrabble-ef26c.appspot.com',
+    apiKey: 'AIzaSyDZsauApwqMD4lPHg9X187i3kUv5bva0Pw',
+    authDomain: 'superscrabble-ef26c.firebaseapp.com',
+    messagingSenderId: '735949795375',
+    measurementId: 'G-7M6Q6WZS1G',
+  },
   production: false
 };
 
