6b2a763eba4dd431d14ef8c673b278b20ae7a9c8
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-03-04 19:18:42 +0200

    Add Remote Config to LeaveGameDialog

--- src/ClientApp/super-scrabble-app/src/app/pages/common/dialogs/leave-game-dialog/leave-game-dialog.component.ts
+++ src/ClientApp/super-scrabble-app/src/app/pages/common/dialogs/leave-game-dialog/leave-game-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { AngularFireRemoteConfig } from '@angular/fire/compat/remote-config';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 
@@ -9,7 +10,26 @@ import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 })
 export class LeaveGameDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ErrorDialogComponent>) {}
+  leavePopupText: string = "";
+  leaveComfirmBtnLabel: string = "";
+  leaveDenyBtnLabel: string = "";
+
+  constructor(public dialogRef: MatDialogRef<ErrorDialogComponent>,
+              private remoteConfig: AngularFireRemoteConfig) {
+    this.loadRemoteConfigTexts();
+  }
+
+  private loadRemoteConfigTexts() {
+    //AppConfig.isRemoteConfigFetched = false;
+    this.remoteConfig.fetchAndActivate().then(hasActivatedTheFetch => {
+      this.remoteConfig.getAll().then(all => {
+        //AppConfig.isRemoteConfigFetched = true;
+        this.leavePopupText = all["LeavePopupText"].asString()!;
+        this.leaveComfirmBtnLabel = all["LeaveComfirmBtnLabel"].asString()!;
+        this.leaveDenyBtnLabel = all["LeaveDenyBtnLabel"].asString()!;
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
