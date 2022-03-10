91ef604de503115f304580f614608a14d0517a76
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-03-10 18:03:29 +0200

    Word on Loading; for now - stopped

--- src/ClientApp/super-scrabble-app/src/app/services/loading-screen.service.ts
+++ src/ClientApp/super-scrabble-app/src/app/services/loading-screen.service.ts
@@ -1,33 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnChanges, SimpleChanges } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoadingScreenService {
+export class LoadingScreenService implements OnChanges {
 
   isShowingLoading: boolean = false;
 
+  lastLoadingDontShowTime: number = Number.MAX_SAFE_INTEGER;
+  lastLoadingShowTime: number = Number.MAX_SAFE_INTEGER;
+
   constructor() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ON CHANGES " + this.isShowingLoading);
+    console.log(changes)
+  }
+
   showLoadingScreen() {
+    console.log("Show" + " " + Date.now())
     if(this.isShowingLoading) return;
 
     setTimeout(() => {
-      this.isShowingLoading = true;
+      this.lastLoadingShowTime = Date.now();
+      //this.isShowingLoading = true;
     }, 0)
     
   }
 
   stopShowingLoadingScreen() {
+    console.log("Don't Show" + " " + Date.now())
     if(!this.isShowingLoading) return;
 
     setTimeout(() => {
+      this.lastLoadingDontShowTime = Date.now();
       this.isShowingLoading = false;
     }, 0)
     //this.isShowingLoading = false;
   }
 
   isShowingLoadingScreen() : boolean {
-    return this.isShowingLoading;
+    //console.log("IS SHOWING CALLED " + this.isShowingLoading + " " + Date.now());
+    //if(this.lastLoadingDontShowTime >= this.lastLoadingShowTime) {
+      //console.log("FALSE" + this.lastLoadingDontShowTime + " " + this.lastLoadingShowTime);
+      return false;
+    //}
+    //console.log("TRUE" + this.lastLoadingDontShowTime + " " + this.lastLoadingShowTime);
+    //return true;
+
+    //return this.isShowingLoading;
   }
 }
