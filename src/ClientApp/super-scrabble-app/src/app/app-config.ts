3650ed692d0568aba880cc126a180a63359a6ec1
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2022-03-23 13:07:17 +0200

    Code clean-up

--- src/ClientApp/super-scrabble-app/src/app/app-config.ts
+++ src/ClientApp/super-scrabble-app/src/app/app-config.ts
@@ -2,6 +2,4 @@ export class AppConfig {
     public static readonly WildcardSymbol: string = "\u0000";
     public static readonly BoardCellMinWidth: number = 35.2; //2.2rem = 35.2px
     public static readonly BoardCellMinHeight: number = 35.2; //2.2rem = 35.2px
-    public static readonly ServerUrl: string = "https://localhost:7168";
-    public static readonly ServerPort: string = "";
 }
